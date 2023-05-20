4e530a2a68d5c37285622d2fbacc20536ceb74fa
Niceiyke <oyomworld@gmail.com>
2023-05-20 14:17:57 +0100

    ee

--- scrapy_jumia/pipelines.py
+++ scrapy_jumia/pipelines.py
@@ -13,62 +13,79 @@ import psycopg2
 
 class JumiaPipeline:
     def process_item(self, item, spider):
-        print('pipeline' +item['name'])
+        print("pipeline" + item["name"])
         return item
-    
+
+
 class Remove_Items_NotinStock_Pipeline:
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
 
-    def process_item(self,item,spider):
-        adapter =ItemAdapter(item)
-
-        if adapter['stock']== "Add To Cart":
+        if adapter["stock"] == "Add To Cart":
             return item
-            
+
         else:
-            raise DropItem(' {item} sold out')
-            
+            raise DropItem(" {item} sold out")
+
+
 class Remove_Items_withNoDiscount_Pipeline:
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
 
-    def process_item(self,item,spider):
-        adapter =ItemAdapter(item)
-
-
-        if adapter['original_price'] is not None:
+        if adapter["original_price"] is not None:
             return item
-            
+
         else:
-            raise DropItem('No Discount found for {item}')
-        
+            raise DropItem("No Discount found for {item}")
+
 
 class SavingToDb:
-
     def __init__(self):
-        self.con =sqlite3.connect('scrapy.db')
+        self.con = sqlite3.connect("scrapy.db")
 
-        self.cur =self.con.cursor()
+        self.cur = self.con.cursor()
         self.create_table()
 
     def create_table(self):
+        self.cur.execute(
+            """CREATE TABLE IF NOT EXISTS products(
+        name TEXT PRIMARY KEY,stock text,category text,store text,image text,url text,discount REAL,original_price REAL,discount_price REAL)"""
+        )
 
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS products(
-        name TEXT PRIMARY KEY,stock text,category text,store text,image text,url text,discount REAL,original_price REAL,discount_price REAL)""")
+    def process_item(self, item, spider):
+        self.cur.execute(
+            """ INSERT INTO products VALUES (?,?,?,?,?,?,?,?,?)""",
+            (
+                item["name"],
+                item["stock"],
+                item["category"],
+                item["store"],
+                item["image"],
+                item["url"],
+                item["discount_percent"],
+                item["original_price"],
+                item["discount_price"],
+            ),
+        )
 
-    def process_item(self,item,spider):
-        self.cur.execute(""" INSERT INTO products VALUES (?,?,?,?,?,?,?,?,?)""",
-                         (item['name'],item['stock'],item['category'],item['store'],item['image'],item['url'],item['discount_percent'],item['original_price'],item['discount_price'],))
-        
         self.con.commit()
 
         return item
-    
+
+
 class SavingToDbpostgres:
-
     def __init__(self):
-        self.con = psycopg2.connect(database="postgres",user='postgres',password='postgres',host='3.23.130.169',port= '5432')
-        
-        self.cur=self.con.cursor()
+        self.con = psycopg2.connect(
+            database="postgresec2",
+            user="postgresec2",
+            password="postgresec2",
+            host="3.23.130.169",
+            port="5432",
+        )
 
-        print('connected')
+        self.cur = self.con.cursor()
+
+        print("connected")
 
     '''
     def create_table(self):
@@ -87,6 +104,4 @@ class SavingToDbpostgres:
             print('db_err',e)
 
         return item
-    '''    
-
-    
+    '''
